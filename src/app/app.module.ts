2399566a549062c47b3dfabcc952c64acc8ed9af
xuncl <xuncl@126.com>
2017-11-17 10:47:25 +0800

    事件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,32 @@
 import { BrowserModule } from '@angular/platform-browser'; // 浏览器解析模块
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component'; // 自定义模块
 import { HeaderComponent } from './components/header/header.component';
 import { NewsComponent } from './components/news/news.component';
+import { New2Component } from './components/new2/new2.component';
+
+import { StorageService } from './services/storage.service'
 
 
 /* nNgModule装饰器将AppModule标记为Angular模块类（也叫NgModule类）。
-     NgModule, 接受一元数组对告诉Angular如何编译和启动应用。*/
+     NgModule, 接受一元数组�,
+    �告诉Angular如何编译和启动应用。*/
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NewsComponent
+    NewsComponent,
+    New2Component
   ],
   imports: [ // 当前的项目依赖哪些模块
-    BrowserModule
+    BrowserModule, FormsModule
   ],
-  providers: [], // 定义的Providers 服务
+  providers: [StorageService], // 定义的Providers 服务
   bootstrap: [AppComponent] // 默认启动哪个组件
 })
 
--- src/app/components/new2/new2.component.ts
+++ src/app/components/new2/new2.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+import { StorageService } from '../../services/storage.service'
+
 @Component({
   selector: 'app-new2',
   templateUrl: './new2.component.html',
@@ -8,9 +10,49 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class New2Component implements OnInit {
 
-  constructor() { }
+  public msg:any;
+  public username:any;
+  public flag:any;
+  public search:any;
+
+  public storage= new StorageService();
+
+  constructor() {
+    this.msg="这是另一个新闻组件。"
+    this.username="name";
+    this.flag=true;
+    this.search="";
+   }
 
   ngOnInit() {
   }
 
+  getMsg(){
+    // 获取属性的值
+    alert(this.msg);
+  }
+
+  setUsername(){
+    this.username="new name"
+  }
+
+  toggleFlag(){
+    this.flag=!this.flag;
+  }
+
+  keyupFn(e){
+    console.log("Key up now!")
+    console.log(e)
+    if (e.keyCode==13){
+      alert("hui che")
+    }
+  }
+
+  onClick(e){
+    console.log(e);
+  }
+
+  getSearch(){
+    alert(this.search);
+  }
 }
